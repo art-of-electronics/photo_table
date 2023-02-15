243c73567c133ee17a800a4f263ad91102914a69
ARO <121867860+art-of-electronics@users.noreply.github.com>
2023-02-15 23:44:31 +0100

    Update main_wemos_d1mini.py

--- main_wemos_d1mini.py
+++ main_wemos_d1mini.py
@@ -17,65 +17,68 @@ ms1.off()
 ms2 = Pin(4, Pin.OUT)
 ms2.off()
 dr = Pin(15, Pin.OUT)
-pwm = PWM(Pin(0), duty = 0)
+pwm = PWM(Pin(0), duty=0)
 
-class cnt:
-    global sw1
-    global sw2
-    global sw3
-    global ms_mode
-    global f
+
+class Cnt:
     sw1 = False
     sw2 = False
     sw3 = False
     ms_mode = 0
     f = 1000
 
+
+last_sw1 = last_sw2 = last_sw3 = 0
+
+
 def toggle_led():
-    led.value(led.value()^1)
-    en.value(en.value()^1)
+    led.value(led.value() ^ 1)
+    en.value(en.value() ^ 1)
     if en.value() == 0:
         pwm.duty(512)
     else:
         pwm.duty()
 
+
 def key_dir():
-    global sw1
-    last_sw = sw1
-    sw1 = lr_dir.value()
-    if last_sw and not sw1:
-        dr.value(dr.value()^1)
-       
+    global last_sw1
+    last_sw1 = Cnt.sw1
+    Cnt.sw1 = lr_dir.value()
+    if last_sw1 and not Cnt.sw1:
+        dr.value(dr.value() ^ 1)
+
+
 def key_run():
-    global sw2
-    last_sw = sw2
-    sw2 = run.value()
-    if last_sw and not sw2:
+    global last_sw2
+    last_sw2 = Cnt.sw2
+    Cnt.sw2 = run.value()
+    if last_sw2 and not Cnt.sw2:
         toggle_led()
-       
+
+
 def key_mode():
-    global sw3
-    global ms_mode
-    last_sw = sw3
-    sw3 = mode.value()
-    if last_sw and not sw3:
-        ms_mode += 1
-        if ms_mode == 1:
+    global last_sw3
+    last_sw3 = Cnt.sw3
+    Cnt.sw3 = mode.value()
+    if last_sw3 and not Cnt.sw3:
+        Cnt.ms_mode += 1
+        if Cnt.ms_mode == 1:
             ms1.on()
             ms2.on()
-        elif ms_mode == 2:
+        elif Cnt.ms_mode == 2:
             ms1.on()
             ms2.off()
-        elif ms_mode == 3:
+        elif Cnt.ms_mode == 3:
             ms1.off()
             ms2.on()
         else:
             ms1.off()
             ms2.off()
-            ms_mode = 0
+            Cnt.ms_mode = 0
+
 
 while True:
-    f = 500 + ((int(adc.read()/100))*80)
+    f = 500 + ((int(adc.read() / 100)) * 80)
     pwm.freq(f)
     key_dir()
     key_run()
